c1468e0212c0977ebc0ad58dbdd987f330ab76b5
JuliaThTranNguyen <trhien541@gmail.com>
2023-12-12 20:38:23 +0200

    testing 7

---
+++ index.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import 'dotenv-expand/config';
+import express from 'express';
+import { router } from './routes';
+import { routeNotFound } from './middlewares/routeNotFound';
+import { loggingMiddleware } from './middlewares/logging';
+import { errorLoggingMiddleware } from './middlewares/error';
+import { connectMongoDB } from './config/mongoose';
+
+const app = express();
+
+connectMongoDB();
+
+// Body parser middleware for application/json
+app.use(express.json());
+
+app.use(loggingMiddleware);
+// So we can also do /api/v1/ or /api/v2/ etc.
+app.use('/api/v1/', router);
+app.use(errorLoggingMiddleware);
+app.use(routeNotFound);
+
+const PORT = process.env.PORT ?? 1337;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is accessible at http://localhost:${PORT}`);
+});
+
+export default app;
